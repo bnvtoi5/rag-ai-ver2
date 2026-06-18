0bf99bb5d1c12b74b2ae71dcbb6f96e05db6cc44
bnvtoi5 <bnvtoi5@gmail.com>
2026-06-19 00:39:54 +0700

    Update embeddings.py

--- src/database/embeddings.py
+++ src/database/embeddings.py
@@ -1,8 +1,12 @@
+import streamlit as sn # Đổi tên alias tùy ý hoặc dùng import streamlit as st
+import streamlit as st
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from src.config import Config
 
+@st.cache_resource
 def get_embedding_model():
-    # Mô hình siêu nhẹ ~120MB, tối ưu hoàn hảo cho máy RAM 8GB
+    """Tải và lưu cache mô hình embedding để tránh tải lại nhiều lần làm tràn RAM"""
+    print("⏳ Đang khởi tạo mô hình Embedding (Chỉ chạy 1 lần duy nhất)...")
     return HuggingFaceEmbeddings(
         model_name=Config.EMBEDDING_MODEL,
         model_kwargs={'device': 'cpu'}
